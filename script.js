22e42727c8d1fdab46d7d66215cdf8336bdf6ce2
Chuka Ukekwe <ukekwe.chuka@gmail.com>
2021-07-05 15:08:18 -0400

    finished the javascript coding segement. Note to look up on more on the regex syntax

--- script.js
+++ script.js
@@ -61,12 +61,60 @@ result.addEventListener('click', function () {
     let inputString = input.innerHTML;
 
     //forming an array of numbers
-    let numbers = inputString.split(/\+|\-|\×|\÷/g);
+    let numbers = inputString.split(/\+|\-|\×|\÷/g);   //what is this strange syntax? Why does it work?
 
     //forming an array of operators
     //first we replace all the numbers and dot with empty strying and then split
     let operators = inputString.replace(/[0-9]|\./g, "").split("");
 
+    console.log(inputString);
+    console.log(operators);
+    console.log(numbers);
+    console.log("------------------------");
 
+    //now we are looping through the array and doing one operation at a time.
+    //first divide, multiply, then subtraction and then addition
+    //as we move we are alternating the original numbers and operators array
+    //the final element remaining in the array will be the output
+
+    let divide = operators.indexOf("÷");
+    while (divide != -1) {
+        numbers.splice(divide, 2, numbers[divide] / numbers[divide + 1])
+        operators.splice(divide, 1);
+        divide = operators.indexOf("÷");
+    }
+
+    let multiply = operators.indexOf("×");
+    while (multiply != -1) {
+        numbers.splice(multiply, 2, numbers[multiply] * numbers[multiply + 1]);
+        operators.splice(multiply, 1);
+        multiply = operators.indexOf("×");
+    }
+
+    let subtract = operators.indexOf("-");
+    while (subtract != -1) {
+        numbers.splice(subtract, 2, numbers[subtract] - numbers[subtract + 1]);
+        operators.splice(subtract, 1);
+        subtract = operators.indexOf("-");
+    }
+
+    let add = operators.indexOf("+");
+    while (add != -1) {
+        // using parseFloat is necessary, otherwise it will result in string concatenation :)
+        numbers.splice(add, 2, parseFloat(numbers[add]) + parseFloat(numbers[add + 1]));
+        operators.splice(add, 1);
+        add = operators.indexOf("+");
+    }
+
+    input.innerHTML = numbers[0]; //displays the output
+
+    resultDisplayed = true; //turning off the the flag if the result is displayed
+
+});
+
+
+//clearing the input on press of clear
+clear.addEventListener("click", function () {
+    input.innerHTML = "";
 })
 
